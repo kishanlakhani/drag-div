a4e5872c4fe6004025217b54c29eacc8025d1ade
kishan <kishan.lakhani@bacancytechnology.com>
2020-07-10 12:12:40 +0530

    class

--- src/app/canvas-map-image/canvas-map-image.component.ts
+++ src/app/canvas-map-image/canvas-map-image.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterContentInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-canvas-map-image',
   templateUrl: './canvas-map-image.component.html',
   styleUrls: ['./canvas-map-image.component.scss']
 })
-export class CanvasMapImageComponent implements OnInit {
+export class CanvasMapImageComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('myCanvas') myCanvas: ElementRef<HTMLCanvasElement>;
+  public context: CanvasRenderingContext2D;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+
+
+
+  ngAfterViewInit(): void {
+    this.context = this.myCanvas.nativeElement.getContext('2d');
+  }
+
 
 }
